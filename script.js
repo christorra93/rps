2b9bbbae12ccc020313e54a48598e2ad97925083
christorra93 <christorra93@gmail.com>
2023-08-18 15:45:26 -0300

    Added a function to get the computers choice

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function getRandomNum() {
     return Math.floor(Math.random * 3);
 }
 
+function getComputerChoice() {
+    switch (getRandomNum()) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+console.log(getComputerChoice);
