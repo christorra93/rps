eacfc3bce1a1560c6711f222e35d804730db0fb6
christorra93 <christorra93@gmail.com>
2023-08-18 17:13:50 -0300

    Added game function and added a win and lose counter to round function

--- script.js
+++ script.js
@@ -20,32 +20,61 @@ function playRound(player,com) {
         case 'rock':
             if (com === 'scissors') {
                 console.log("You win! rock beats scissors.");
+                win++;
             } else if (com === 'paper') {
                 console.log("You lose! paper beats rock.");
+                lose++;
             }
             break;
         case 'paper':
             if (com === 'rock') {
                 console.log("You win! paper beats rock.");
+                win++;
             } else if (com === 'scissors') {
                 console.log("You lose! scissors beats paper.");
+                lose++;
             }
             break;
         case 'scissors':
             if (com === 'paper') {
                 console.log("You win! scissors beats paper.");
+                win++;
             } else if (com === 'rock') {
                 console.log("You lose! rock beats scissors.");
+                lose++;
             }
             break;
     }
+
     if (player === com) {
         console.log("It's a tie!");
     }
 }
 
-let player = prompt("Rock paper or scissors?","").toLowerCase();
-let computer = getComputerChoice();
+function game() {
 
-playRound(player,computer);
+    let player;
+    let computer;
+    let win = 0;
+    let lose 0;
+
+    for (i = 0 ; i < 5 ; i++) {
+
+        player = prompt("Rock paper or scissors?","").toLowerCase();
+        computer = getComputerChoice();
+
+        playRound(player,computer);
+    }
+
+    if (win > lose) {
+        console.log(`You won the game! ${win} to ${lose}`);
+    } else if (win < lose) {
+        console.log(`You lost the game! ${lose} to ${win}`);
+    } else {
+        console.log("It's a tie!");
+    }
+
+}
+
+game();
 
